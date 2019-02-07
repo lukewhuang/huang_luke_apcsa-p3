b08d128c36779657619ef8118f1e01786a06b64f
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-02-06 17:05:59 -0800

    Unit 6 Completed

--- Unit06/src/Perfect.java
+++ Unit06/src/Perfect.java
@@ -9,17 +9,43 @@ public class Perfect
    private int number;
 
 	//add constructors
-   public void firstCheck() {
-	   
+   public Perfect() {
+	    
+   }
+   public Perfect(int n) {
+	   setNum(n);
+	   isPerfect();
    }
 
 	//add a set method
 
+   public void setNum(int n) {
+	   number = n;
+   }
+   
 	public boolean isPerfect()
 	{
-		return false;
+		int sum = 0; 
+
+		for (int x = 1; x < number; x++) {
+			if (number % x == 0) {
+				sum = sum + x;
+			}
+		}
+		
+		if (sum == number)
+			return true;
+		else
+			return false;
 	}
 
 	//add a toString	
-	
+	public String toString() {
+		if ( isPerfect() ) {
+			return (number + " is perfect.");
+		}
+		else {
+			return (number + " is not perfect.");
+		}
+	}
 }
--- Unit06/src/PerfectRunner.java
+++ Unit06/src/PerfectRunner.java
@@ -8,7 +8,43 @@ public class PerfectRunner
 {
 	public static void main( String args[] )
 	{
+		Perfect p = new Perfect();
 		
+		p.setNum(496);
+		p.isPerfect();
+		System.out.println(p.toString());
+		
+		p.setNum(45);
+		p.isPerfect();
+		System.out.println(p.toString());
+		
+		p.setNum(6);
+		p.isPerfect();
+		System.out.println(p.toString());
+		
+		p.setNum(8128);
+		p.isPerfect();
+		System.out.println(p.toString());
+		
+		p.setNum(1245);
+		p.isPerfect();
+		System.out.println(p.toString());
+		
+		p.setNum(33);
+		p.isPerfect();
+		System.out.println(p.toString());
+		
+		p.setNum(28);
+		p.isPerfect();
+		System.out.println(p.toString());
+		
+		p.setNum(27);
+		p.isPerfect();
+		System.out.println(p.toString());
+		
+		p.setNum(33550336);
+		p.isPerfect();
+		System.out.println(p.toString());
 																
 	}
 }
