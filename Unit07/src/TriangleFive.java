9b8e2f87c1bb2582c4aba9f895140d151eabe6ee
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-02-13 00:19:46 -0800

    unit 7

--- Unit07/src/TriangleFive.java
+++ Unit07/src/TriangleFive.java
@@ -11,23 +11,54 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		setLetter('x');
+		setAmount(0);
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		setLetter(c);
+		setAmount(amt);
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
 
 	public String toString()
 	{
+
 		String output="";
+		
+		for (int i = amount; i > 0 ; i--)
+		{
+			int x = 0;
+			char newLetter = letter;
+			
+			while (x<i) {
+				for (int z = amount - x ; z>0; z--) {
+					output = newLetter + output;
+				}
+				
+				if(newLetter == 'Z' || newLetter == 'z') {
+					newLetter -= 25;
+				}
+				else
+					newLetter++;
+				
+				x++;
+			}
+			
+			output = output + "\n";
+		}
+		
+		
 		return output;
 	}
 }
