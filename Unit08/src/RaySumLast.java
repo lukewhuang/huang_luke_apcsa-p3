be59d963788ec238df1155728eb100edd7ecc7ef
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-02-14 13:15:39 -0800

    Unit 8 in progress

--- Unit08/src/RaySumLast.java
+++ Unit08/src/RaySumLast.java
@@ -5,8 +5,28 @@
 
 public class RaySumLast
 {
+	public RaySumLast() {
+		go(new int[] {0} );
+	}
+	
+	public RaySumLast(int[] array) {
+		go(array);
+	}
+
 	public static int go(int[] ray)
 	{
-		return 0;
+		int total = 0;
+		int none = 0;
+		
+		for (int i = 0; i < ray.length; i++) {
+			if (ray[i] > ray [ray.length-1]){
+				total += ray[i];
+				none++;
+			}
+		}
+		if (none == 0)
+			return -1;
+		else	
+			return total;
 	}
 }
--- Unit08/src/SumLastRunner.java
+++ Unit08/src/SumLastRunner.java
@@ -6,6 +6,13 @@
 public class SumLastRunner
 {
 	public static void main( String args[] )
-	{											
+	{		
+		int [] array = new int[] {-99,1,2,3,4,5,6,7,8,9,10,5};
+		
+		System.out.println(RaySumLast.go(array));
+		
+		int [] array2 = new int[] {10,9,8,7,6,5,4,3,2,1,-99};
+		
+		System.out.println(RaySumLast.go(array2));
 	}
 }
