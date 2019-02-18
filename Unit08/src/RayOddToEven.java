b7381a6ddaed012ede8225b2373c03453ca128ab
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-02-18 14:19:39 -0800

    Unit 8 completed

--- Unit08/src/RayOddToEven.java
+++ Unit08/src/RayOddToEven.java
@@ -5,8 +5,32 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		int odd =0;
+		int even = 0;
+		int oddDistance = 0;
+		int evenDistance = 0;
+		
+		for (int i=0; i < ray.length-1 ; i++) {
+			if (ray[i]%2 != 0) {
+				oddDistance = i;
+				odd = ray[i];
+				break;
+			}
+		}
+		for (int i = oddDistance ; i<ray.length; i++) {
+			if (ray[i]%2 == 0)
+			{
+				evenDistance = i;
+				even = ray[i];
+				break;
+			}
+		}
+		
+		if (evenDistance > oddDistance) 
+			return (evenDistance - oddDistance);
+		else
+			return (-1);
 	}
 }
