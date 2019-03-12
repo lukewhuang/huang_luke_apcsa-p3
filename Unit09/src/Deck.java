156d03316956c39a057da83a5f029cd8bd42760e
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-03-11 22:30:04 -0700

    unit 9 changed to array lists

--- Unit09/src/Deck.java
+++ Unit09/src/Deck.java
@@ -9,16 +9,13 @@ import java.util.ArrayList;
  */
 public class Deck {
 
-	/**
-	 * cards contains all the cards in the deck.
-	 */
-	private Card[] cards;
 
-	/**
-	 * size is the number of not-yet-dealt cards.
-	 * Cards are dealt from the top (highest index) down.
-	 * The next card to be dealt is at size - 1.
-	 */
+	
+	// ARRAY VERSION --> private Card[] cards;
+
+	private List<Card> cards = new ArrayList<Card>();
+	
+	
 	private int size;
 
 
@@ -32,6 +29,9 @@ public class Deck {
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
 		
+		
+		/* ARRAY VERSION
+		
 		size = ranks.length * suits.length;
 		cards = new Card[size];
 		
@@ -42,31 +42,37 @@ public class Deck {
 				counter++;
 			}
 		}
+		
+		*/
+		
+		
+		// ARRAY LIST VERSION
+		
+		for (int i=0; i<suits.length; i++)
+		{
+			for (int x =0; x<ranks.length; x++)
+			{
+				cards.add(new Card(ranks[x], suits[i], values[x]));
+			}
+		}
+		
+		size = cards.size();
+		
 	}
 
-
-	/**
-	 * Determines if this deck is empty (no undealt cards).
-	 * @return true if this deck is empty, false otherwise.
-	 */
 	public boolean isEmpty() {
 		return (size == 0);
 	}
 
-	/**
-	 * Accesses the number of undealt cards in this deck.
-	 * @return the number of undealt cards in this deck.
-	 */
+
 	public int size() {
 		return size;
 	}
 
-	/**
-	 * Randomly permute the given collection of cards
-	 * and reset the size to represent the entire deck.
-	 */
 	public void shuffle() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
+		/* ARRAY VERSION
+		
 		Card[] cardArr = new Card[cards.length];
 		Random rand = new Random();
 		int position = 0;
@@ -76,14 +82,22 @@ public class Deck {
 			cardArr[i] = cardArr[position];
 			cardArr[position] = temp;
 		}
+		
+		
+		*/
+		
+		for (int i=cards.size()-1; i >= 1; i--)
+		{
+			int r = (int)(Math.random() * (i+1));
+			Card save = cards.get(i);
+			cards.set(i,  cards.get(r));
+			cards.set(r,  save);
+		}
 	}
 
-	/**
-	 * Deals a card from this deck.
-	 * @return the card just dealt, or null if all the cards have been
-	 *         previously dealt.
-	 */
 	public Card deal() {
+		/* ARRAY VERSION
+		
 		if (size>0) {
 			size--;
 		}
@@ -91,16 +105,22 @@ public class Deck {
 			return null;
 		
 		return cards[size];
+		
+		*/ 
+		
+		//ARRAY LIST VERSION
+		if (cards.size() <= 0)
+			return null;
+		size -= 1;
+		return cards.get(size);
 	}
 
-	/**
-	 * Generates and returns a string representation of this deck.
-	 * @return a string representation of this deck.
-	 */
 	@Override
 	public String toString() {
 		String rtn = "size = " + size + "\nUndealt cards: \n";
-
+		
+		
+		/* ARRAY VERSION
 		for (int k = size - 1; k >= 0; k--) {
 			rtn = rtn + cards[k];
 			if (k != 0) {
@@ -124,6 +144,21 @@ public class Deck {
 			}
 		}
 
+		rtn = rtn + "\n";
+		return rtn;
+		*/
+		
+		rtn = rtn + "\nDealt cards: \n";
+		for (int k = cards.size() - 1; k>=size; k--)
+		{
+			rtn = rtn + cards.get(k);
+			if (k != size)
+				rtn = rtn + ", ";
+			
+			if ((k-size) %2 == 0)
+				rtn =rtn + "\n";
+		}
+		
 		rtn = rtn + "\n";
 		return rtn;
 	}
