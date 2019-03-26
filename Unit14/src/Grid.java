9b8850f4daf162166be866467a34aac2ffa4c830
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-03-25 20:09:58 -0700

    Unit 14 completed

--- Unit14/src/Grid.java
+++ Unit14/src/Grid.java
@@ -2,6 +2,8 @@
 //www.apluscompsci.com
 //Name -
 
+import java.util.Arrays;
+
 public class Grid
 {
    private String[][] grid;
@@ -9,24 +11,53 @@ public class Grid
    //load vals into the rows x cols grid randomly
 	public Grid(int rows, int cols, String[] vals)
 	{
+		grid = new String[rows][cols];
+		for (int row = 0; row < grid.length; row++)
+		{
+			for (int col = 0; col < grid[row].length; col++)
+			{
+				grid[row][col] = vals[(int)(Math.random() * vals.length)];
+			}
+		}
 	}
 
 	//find out which of the vals occurs the most
 	public String findMax(String[] vals)
 	{
-		return "nothing yet";
+		String str = vals[0];
+		for (String s : vals)
+		{
+			if (countVals(s) > countVals(str))
+				str = s;
+		}
+		
+		return str;
 	}
 
 	//returns a count of how many times val occurs in the matrix
 	private int countVals( String val )
 	{
-		return 0;
+		int count = 0;
+		
+		for (int row = 0 ; row < grid.length; row++)
+		{
+			for (int col = 0 ; col < grid[row].length; col++)
+				if (grid[row][col].contentEquals(val))
+					count++;
+		}
+				
+		return count;
 	}
 
 	//display the grid
 	public String toString()
 	{
-		String output="";
+		String output = "";
+		for (int row = 0 ; row < grid.length; row++)
+		{
+			output += "\n" + Arrays.deepToString(grid[row]);
+		}
+		
 		return output;
 	}
 }
--- Unit14/src/GridRunner.java
+++ Unit14/src/GridRunner.java
@@ -11,5 +11,12 @@ public class GridRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
+		Grid test = new Grid(7, 7, new String[] {"a", "b", "c", "d", "e", "f", "g"});
+		System.out.println(test);
+		System.out.println(test.findMax( new String[] {"a", "b", "c", "d", "e", "f", "g"}) + " occurs the most.");
+		
+		Grid test2 = new Grid(4, 4, new String[] {"a", "b", "c", "d"});
+		System.out.println(test2);
+		System.out.println(test2.findMax( new String[] {"a", "b", "c", "d"}) + " occurs the most.");
 	}
 }
