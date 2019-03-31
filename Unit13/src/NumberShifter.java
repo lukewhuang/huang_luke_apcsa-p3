c369c8a3912f5b6ff4e05a2742cb8f000b8036f3
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-03-30 23:29:48 -0700

    unit 13 completed

--- Unit13/src/NumberShifter.java
+++ Unit13/src/NumberShifter.java
@@ -12,9 +12,24 @@ public class NumberShifter
 {
 	public static int[] makeLucky7Array( int size)
 	{
-		return null;
+		int[] arr = new int[size];
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = (int) (Math.random() * 10 + 1);
+		}
+		return arr;
 	}
 	public static void shiftEm(int[] array)
 	{
+		for (int i=1; i <array.length; i++)
+		{
+			int j = i; 
+			while (j > 0 && array[j-1] != 7 && array[j] == 7)
+			{
+				int temp = array[j-1];
+				array[j] = temp;
+				j--;
+			}
+		}
 	}
 }
