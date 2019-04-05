7818c254e87ea95da2e4497637f9302a649f520f
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-04-05 11:56:32 -0700

    unit 15 in progress

--- Unit15/src/Block.java
+++ Unit15/src/Block.java
@@ -16,57 +16,121 @@ public class Block implements Locatable
 
 	public Block()
 	{
-
+		xPos = 0;
+		yPos = 0;
+		width = 0;
+		height = 0;
 
 	}
+	
+	public Block(int x, int y)
+	{
+		setX(x);
+		setY(y);
+	}
+	
+	public Block (int x, int y, int w, int h)
+	{
+		setX(x);
+		setY(y);
+		setWidth(w);
+		setHeight(h);
+	}
+	
+	public Block (int x, int y, int w, int h, Color col)
+	{
+		setX(x);
+		setY(y);
+		setWidth(w);
+		setHeight(h);
+		setColor(col);
+	}
 
-	//add other Block constructors - x , y , width, height, color
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
    //add the other set methods
+   public void setX(int x)
+   {
+	   xPos = x;
+   }
    
+   public void setY(int y)
+   {
+	   yPos = y;
+   }
 
+   public void setWidth(int w)
+   {
+	   width = w;
+   }
+   
+   public void setHeight(int h)
+   {
+	   height = h;
+   }
+   
    public void setColor(Color col)
    {
-
-
+	   color = col;
    }
 
    public void draw(Graphics window)
    {
-   	//uncomment after you write the set and get methods
-      //window.setColor(color);
-      //window.fillRect(getX(), getY(), getWidth(), getHeight());
+   	  window.setColor(color);
+      window.fillRect(getX(), getY(), getWidth(), getHeight());
    }
 
    public void draw(Graphics window, Color col)
    {
-
+	   window.setColor(col);
+	   window.fillRect(getX(), getY(), getWidth(), getHeight());
 
    }
    
 	public boolean equals(Object obj)
 	{
-
-
-
-
-		return false;
+		Block other = (Block) obj;
+		if (xPos == other.getX() && yPos == other.getY()
+				&& width == other.getWidth() && height == other.getHeight()
+				&& color.equals(other.getColor()));
+			return true;
 	}   
 
    //add the other get methods
+    public int getX()
+    {
+    	return xPos;
+    }
     
+    public int getY()
+    {
+    	return yPos;
+    }
+    
+    public int getWidth()
+    {
+    	return width;
+    }
 
+    public int getHeight()
+    {
+    	return height;
+    }
+    
+    public Color getColor()
+    {
+    	return color;
+    }
    //add a toString() method  - x , y , width, height, color
+    
+    
+    public String toString()
+    {
+    	return xPos + " " + yPos + " " + width + " " + height + " " + color.toString();
+    }
+
+@Override
+public void setPos(int x, int y) {
+	// TODO Auto-generated method stub
+	
+}
 }
--- Unit15/src/Paddle.java
+++ Unit15/src/Paddle.java
@@ -12,36 +12,74 @@ public class Paddle extends Block
 
    public Paddle()
    {
-		super(10,10);
-      speed =5;
+	  super(10,10);
+      speed = 5;      
+   }
+   
+   public Paddle(int x, int y)
+   {
+	   super(x,y);
+	   speed = 5;
+   }
+   
+   
+   public Paddle(int x, int y, int z)
+   {
+	   super (x,y);
+	   setSpeed(z);
    }
 
-
+   public Paddle(int x, int y, int w, int h)
+   {
+	   super(x, y, w, h);
+	   setSpeed(5);
+   }
+   
+   public Paddle(int x, int y, int w, int h, int z)
+   {
+	   super(x, y, w, h);
+	   setSpeed(z);
+   }
+   
+   public Paddle(int x, int y, int w, int h, Color col, int z)
+   {
+	   super(x, y, w, h, col);
+	   setSpeed(z);
+   }
    //add the other Paddle constructors
 
-
-
-
-
-
-
-
+   public void setSpeed(int s)
+   {
+	   speed = s;
+   }
 
 
    public void moveUpAndDraw(Graphics window)
    {
-
+	   draw(window, Color.WHITE);
+	   setY(getY() - speed);
+	   draw(window);
 
    }
 
    public void moveDownAndDraw(Graphics window)
    {
-
+	   draw(window, Color.WHITE);
+	   setY(getY() - speed);
+	   draw(window);
 
    }
 
    //add get methods
-   
+   public int getSpeed()
+   {
+	   return speed;
+   }
    
    //add a toString() method
+   
+   public String toString()
+   {
+	   return getX() + " " + getY() + " " + getWidth() + " " + getHeight() + " " + getColor() + " " + getSpeed();
+   }
 }
