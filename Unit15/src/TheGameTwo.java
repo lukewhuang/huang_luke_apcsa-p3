24b81f8b2de5699e52fdafcfedc82a2380137b4b
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-04-29 13:17:29 -0700

    pong extension started

---
+++ Unit15/src/TheGameTwo.java
@@ -0,0 +1,32 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import javax.swing.JFrame;
+import java.awt.Component;
+
+public class TheGameTwo extends JFrame
+{
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+
+	public TheGameTwo()
+	{
+		super("BREAKOUT");
+		setSize(WIDTH,HEIGHT);
+
+		Breakout game = new Breakout();
+		
+		((Component)game).setFocusable(true);
+		getContentPane().add(game);
+
+		setVisible(true);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main( String args[] )
+	{
+		TheGameTwo run = new TheGameTwo();
+	}
+}
