f5a57f9302c7b4b23a0692e7b2750305e2f662a7
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-04-30 13:22:11 -0700

    Pong Extension first level completed

--- Unit15/src/Breakout.java
+++ Unit15/src/Breakout.java
@@ -14,6 +14,7 @@ import java.awt.event.KeyEvent;
 import static java.lang.Character.*;
 import java.awt.image.BufferedImage;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Breakout extends Canvas implements KeyListener, Runnable
@@ -22,29 +23,64 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 	private Paddle paddle;
 	private boolean[] keys;
 	private BufferedImage back;
-	private int rightscore;
-	private int leftscore;
-	private Wall up, down, left, right;
+	
+	private ArrayList<Block> blocks = new ArrayList<Block>();
+	
 	Random rand = new Random();
 
 	public Breakout()
 	{
 		
 		ball = new Ball(600,200,10,10,Color.BLUE,3,2);
-
 		paddle = new Paddle(400, 400, 40, 40,Color.RED,3);
-		
-		up = new Wall(0,0,800,10,Color.GRAY);
-		down = new Wall(0,551,800,10,Color.GRAY);
-		left = new Wall(0,10,10,541,Color.GRAY);
-		right = new Wall(774,10,10,541,Color.GRAY);
-		
-		rightscore = 0;
-		leftscore = 0;
 
 		keys = new boolean[4];
+		
+		
+		//left
+		for (int x = 5; x < 85; x += 40)
+		{
+			for (int y = 5 ; y < 550 ; y += 80)
+			{
+				blocks.add(new Block(x, y, 35, 75, Color.BLUE));
+			}
+		}
+		
+		//right
+		for (int x = 685; x < 750 ; x += 80)
+		{
+			for (int y = 5 ; y < 550 ; y += 80)
+			{
+				blocks.add(new Block(x, y, 35, 75, Color.BLUE));
+			}
+		}
 
-    
+		for (int x = 645; x < 720 ; x += 80)
+		{
+			for (int y = 5 ; y < 550 ; y += 80)
+			{
+				blocks.add(new Block(x, y, 35, 75, Color.BLUE));
+			}
+		}
+		
+		//top
+		for (int x = 85; x < 600 ; x += 80)
+		{
+			for (int y = 5 ; y < 85 ; y += 40)
+			{
+				blocks.add(new Block(x, y, 75, 35, Color.BLUE));
+			}
+		}
+		
+		//bottom
+		for (int x = 85; x < 600 ; x += 80)
+		{
+			for (int y = 525 ; y > 445 ; y -= 40)
+			{
+				blocks.add(new Block(x, y, 75, 35, Color.BLUE));
+			}
+		}
+		
     	setBackground(Color.WHITE);
 		setVisible(true);
 		
@@ -69,86 +105,71 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 
 		ball.moveAndDraw(graphToBack);
 		paddle.draw(graphToBack);
-		up.draw(graphToBack);
-		down.draw(graphToBack);
-		left.draw(graphToBack);
-		right.draw(graphToBack);
-
+		
+		for (Block b : blocks)
+			b.draw(graphToBack);
+		
 		graphToBack.setColor(Color.BLUE);
 		//graphToBack.drawString("SCOREBOARD",350,80);
 
+		if (ball.getX() < 0)
+		{
+			ball.setXSpeed(Math.abs(ball.getXSpeed()));
+		}
+		else if (ball.getX() > getWidth())
+		{
+			ball.setXSpeed(-1 * Math.abs(ball.getXSpeed()));
+		}
+		if (ball.getY() < 0)
+		{
+			ball.setYSpeed(Math.abs(ball.getYSpeed()));
+		}
+		
+		else if (ball.getY() > getHeight())
+		{
+			ball.setYSpeed(-1 * Math.abs(ball.getYSpeed()));
+		}
+		
+		
+		
+		if (ball.didCollideLeft(paddle))
+		{
+			ball.setXSpeed(Math.abs(ball.getXSpeed()));
+		}
+		if (ball.didCollideRight(paddle))
+		{
+			ball.setXSpeed(-1 * Math.abs(ball.getXSpeed()));
+		}
+		if (ball.didCollideBottom(paddle))
+		{
+			ball.setYSpeed(Math.abs(ball.getYSpeed()));
+		}
+		if (ball.didCollideTop(paddle))
+		{
+			ball.setYSpeed(-1 * Math.abs(ball.getYSpeed()));
+		}
+		
+		for (Block b : blocks)
+		{
+			if (ball.didCollideLeft(b) || ball.didCollideRight(b))
+			{
+				b.setColor(Color.WHITE);
+				b.draw(graphToBack);
+				blocks.remove(b);
+				ball.setXSpeed(-ball.getXSpeed());
+			}
+			
+			else if (ball.didCollideTop(b) || ball.didCollideBottom(b))
+			{
+				b.setColor(Color.WHITE);
+				b.draw(graphToBack);
+				blocks.remove(b);
+				ball.setYSpeed(-ball.getYSpeed());
+			}
+		}
+		
 		
 
-		/*if (ball.didCollideLeft(left) || ball.didCollideRight(right))
-		{
-			ball.setXSpeed(0);
-			ball.setYSpeed(0);
-			
-			if (ball.didCollideLeft(left)){
-				rightscore = rightscore + 1;
-			}
-			if (ball.didCollideRight(right)){
-				leftscore = leftscore + 1;
-			}
-			graphToBack.setColor(Color.WHITE);
-			graphToBack.drawString("Left: " + (leftscore-1), 350, 100);
-			graphToBack.drawString("Right: " + (rightscore-1), 350, 120);
-			graphToBack.setColor(Color.BLUE);
-			graphToBack.drawString("Left: " + leftscore, 350, 100);
-			graphToBack.drawString("Right: " + rightscore, 350, 120);
-			
-			ball.draw(graphToBack,Color.WHITE);
-			ball.setX(400);
-			ball.setY(300);
-			ball.setColor(Color.BLUE);
-			int dir = rand.nextInt(2);
-			if (dir == 0){
-				ball.setXSpeed(-(int)(Math.random() * 3 + 2));
-			}
-			else{
-				ball.setXSpeed((int)(Math.random() * 3 + 2));
-			}
-			int ranspeed = (int)(Math.random() * 3);
-			int direction = rand.nextInt(2);
-			if (ranspeed == 0){
-				if (direction == 0){
-					ball.setYSpeed(-(ranspeed+1));
-				}
-				else{
-					ball.setYSpeed((ranspeed+1));
-				}
-			}
-			else{
-				if (direction == 0){
-					ball.setYSpeed(-ranspeed);
-				}
-				else{
-					ball.setYSpeed(ranspeed);
-				}
-			}
-		}
-
-		if (ball.didCollideLeft(paddle)|| ball.didCollideRight(paddle)){
-			ball.setXSpeed(-ball.getXSpeed());
-			
-			
-		}
-		if (ball.didCollideTop(up) || ball.didCollideBottom(down)){
-			ball.setYSpeed(-ball.getYSpeed());
-			
-		}
-		else if (ball.didCollideTop(paddle) || ball.didCollideBottom(paddle)){
-			ball.setYSpeed(-ball.getYSpeed());
-			ball.setXSpeed(-ball.getXSpeed());
-			
-			
-		}
-		else if (ball.didCollideTop(paddle) || ball.didCollideBottom(paddle)){
-			ball.setYSpeed(-ball.getYSpeed());
-			ball.setXSpeed(-ball.getXSpeed());
-			
-		}
-*/
 		if(keys[0] == true)
 		{
 			paddle.moveUpAndDraw(graphToBack);
--- Unit15/src/TheGameTwo.java
+++ Unit15/src/TheGameTwo.java
@@ -7,7 +7,7 @@ import java.awt.Component;
 
 public class TheGameTwo extends JFrame
 {
-	private static final int WIDTH = 800;
+	private static final int WIDTH = 742;
 	private static final int HEIGHT = 600;
 
 	public TheGameTwo()
